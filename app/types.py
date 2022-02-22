b2338200e6862544b9c1864d972c05d078882fe8
Chad Catlett <chad@catlett.info>
2022-02-21 19:36:36 -0600

    Store card ID for sorting and handle cards with multiple classes

--- app/types.py
+++ app/types.py
@@ -3,9 +3,10 @@ from dataclasses import dataclass
 @dataclass
 class Card:
     """Class to represent cards"""
+    Id: int
     Name: str
     ImageURL: str
     Type: str
     Rarity: str
     Set: str
-    Class: str
+    Classes: list[str]
